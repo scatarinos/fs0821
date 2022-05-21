e0cdb560a089ebdfa4a00591762eef75b18f3631
Sergio Catarino <sergio.catarino@tecbased.com>
2022-05-21 18:01:05 +0100

    react spa setup

---
+++ fs0821_react_spa/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mx-auto py-4">
+      <h1>React SPA</h1>
+    </div>
+  );
+}
+
+export default App;
